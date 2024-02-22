649b7783122d92eb2d552c9bf1228b790acf7b47
Equipo8 <equipo1000@email.com>
2024-02-22 10:57:21 -0500

    first commit

---
+++ Empleado/Fecha.py
@@ -0,0 +1,8 @@
+class Fecha:
+    # aqui va todo el codigo de fecha
+    """------------------------------------------
+    #Atributos
+    ---------------------------------------------"""
+    dia = 0
+    mes = 0
+    anio = 0
---
+++ SimuladorBancario/CuentaCoriente.py
@@ -0,0 +1,8 @@
+class cuentacorriente
+    #aqui va el codigo cuenta corriente
+    """----------------------------------------
+    Atributos
+    ------------------------------------------"""
+    cedula= ""
+    nombres= ""
+    mesactual= ""
